7781dfa0ae74b6d67d39c11c764b4c54e4a41b67
ZioGio22 <giovanni.vagnoli@gmail.com>
2021-04-22 11:43:36 +0200

    Finding the error

--- vis.m
+++ vis.m
@@ -19,7 +19,7 @@ VPh=ffreaddata('cyl_VPh.txt');
 %load baseflow
 [Ub] = ffreaddata('Vectored_baseflow.txt');
 [VhP2,ub]=ffvectorget({'P2','P2','P1'}, VPh, Ub, 1);
-[VhP2,vb]=ffvectorget({'P2','P2','P1'}, VPh, Ub, 2);
+[~,vb]=ffvectorget({'P2','P2','P1'}, VPh, Ub, 2);
 [VhP1,pb]=ffvectorget({'P2','P2','P1'}, VPh, Ub, 3);
 %load eigevalues
 id = fopen('eig.txt');
@@ -27,25 +27,40 @@ d = textscan(id,'%f %f');
 e = cell2mat(d);
 fclose('all');
 
+%baseflow visualization
+figure(1)
+ffpdeplot(p,b,t,'VhSeq',VhP2,'XYData',ub,'Mesh','off','MColor','b','Boundary','off','ColorMap',jet);
+title('Baseflow x component')
+xlabel('x')
+ylabel('y')
 %number of eigenvalues calculated
 nEV = length(e(:,1));
 
 %load eigenvectors and build eigenvalues in cartesian form
-la = zeros(1,nEV);
+la = zeros(nEV,1);
 up = zeros(length(ub),nEV);
 vp = zeros(length(vb),nEV);
 pp = zeros(length(pb),nEV);
+
 for i =1:nEV
     la(i) = e(i,1)+1i*e(i,2);
     name = sprintf('vectorized_eigenvectors_%i.txt',i);
-    %     namep=sprintf('p_eigenvector_%i.txt',i);
-    %     nameu=sprintf('u_eigenvector_%i.txt',i);
-    %     namev=sprintf('v_eigenvector_%i.txt',i);
     [Up] = ffreaddata(name);
     [~, up(:,i)]=ffvectorget({'P2','P2','P1'}, VPh, Up, 1);
     [~, vp(:,i)]=ffvectorget({'P2','P2','P1'}, VPh, Up, 2);
     [~, pp(:,i)]=ffvectorget({'P2','P2','P1'}, VPh, Up, 3);
 end
+
+%complex plane plot
+figure(2)
+hold on
+set(gca, 'XAxisLocation', 'origin')
+set(gca, 'YAxisLocation', 'origin')
+plot(la, 'o');
+plot(conj(la), 'or');
+xlabel('Re(\lambda)')
+ylabel('Im(\lambda)')
+grid on
 
 %selecting the eigenvalue withe the greatest real part and the relative
 %modes
@@ -54,34 +69,37 @@ end
 
 lai = la(i);
 upi = up(:,i);
-uvi = vp(:,i);
+vpi = vp(:,i);
 ppi = pp(:,i);
 
 %linearized flow
-A = 25; %a sufficently big amplitude to visualize the perturbation
+A = 0.25; %sufficently large amplitude to visualize the perturbation
 
-% temp = 0:0.1:5;
-% figure
-% for i = 1: length(temp)
-%     u = ub+A*real(upi*exp(lai*temp(i)));
-%     ffpdeplot(p,b,t, ...
-%         'VhSeq',VhP2, ...
-%         'XYData',u, ...
-%         'Mesh','off', ...
-%         'CBTitle','u', ...
-%         'XLim',[-5 20],'YLim',[-5 5], ...
-%         'Title','Instabile mode',...
-%         'MColor','b','Boundary','off','ColorMap',jet);
-%     drawnow
-%     hold on
-% end
+T =2*pi/(imag(lai));
+St=1/T;
+disp(['Numero di Strohual= ', num2str(St)]);
 
-phi=linspace(0,2*pi,50);
+temp = 0:T/100:100;
+ind=2669;
+y=zeros(1,length(temp));
+for i = 1: length(temp)
+    v = vb+A*real(vpi*exp(lai*temp(i)));
+    y(i)=v(ind);
+end
+figure(3)
+plot(temp,y);
+xlabel('t')
+ylabel('v')
+
+T = 50;
+phi=linspace(0,2*pi,T);
 phase=exp(1i*phi);
-clear M
+figure(10)
 for i=1:length(phase)
     utemp=real(up*phase(i));
     vtemp=real(vp*phase(i));
     ffpdeplot(p,b,t,'VhSeq',VhP2,'XYData',utemp,'Mesh','off','MColor','b','Boundary','off','ColorMap',jet);
     drawnow
+    figure(10)
+    hold on
 end
